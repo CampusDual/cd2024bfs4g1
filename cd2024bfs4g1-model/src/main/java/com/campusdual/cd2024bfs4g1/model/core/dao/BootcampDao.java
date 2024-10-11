17be2823e1a9db981b3eb8ee9d84eeff26c5d1fe
DeNgo <ruannicolas_@hotmail.com>
2024-10-11 12:22:56 +0200

    DAO creada

---
+++ cd2024bfs4g1-model/src/main/java/com/campusdual/cd2024bfs4g1/model/core/dao/BootcampDao.java
@@ -0,0 +1,18 @@
+package com.campusdual.cd2024bfs4g1.model.core.dao;
+
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Repository;
+
+import com.ontimize.jee.server.dao.common.ConfigurationFile;
+import com.ontimize.jee.server.dao.jdbc.OntimizeJdbcDaoSupport;
+
+@Repository("BootcampDao")
+@Lazy
+@ConfigurationFile(configurationFile = "dao/BootcampDao.xml", configurationFilePlaceholder = "dao/placeholders.properties")
+public class BootcampDao extends OntimizeJdbcDaoSupport {
+
+    public static final String ATTR_ID = "ID";
+    public static final String ATTR_NAME = "NAME";
+    public static final String ATTR_START_DATE = "START_DATE";
+    public static final String ATTR_END_DATE = "END_DATE";
+}
