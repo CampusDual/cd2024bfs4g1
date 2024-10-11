c2a7e541f0637a30eccca1d69de07a12d04202dd
Mario <marionuman@gmail.com>
2024-10-11 13:15:09 +0200

    Modificado BootcampService

--- cd2024bfs4g1-model/src/main/java/com/campusdual/cd2024bfs4g1/model/core/service/BootcampService.java
+++ cd2024bfs4g1-model/src/main/java/com/campusdual/cd2024bfs4g1/model/core/service/BootcampService.java
@@ -11,6 +11,9 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Map;
+
+@Service("BootcampService")
+@Lazy
 public class BootcampService implements IBootcampService {
 
     @Autowired
