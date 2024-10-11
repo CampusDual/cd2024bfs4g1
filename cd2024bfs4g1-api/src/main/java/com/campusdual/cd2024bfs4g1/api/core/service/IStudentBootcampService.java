4fb99160af55cf3d05a242f7049a7cac058c5dfa
Adrián Fazanes Porto <adrifazanes38@gmail.com>
2024-10-11 15:05:04 +0200

    Grupo D ( David, Javier, Jesus, Adrian ) Parte Backend

--- cd2024bfs4g1-api/src/main/java/com/campusdual/cd2024bfs4g1/api/core/service/IStudentBootcampService.java
+++ cd2024bfs4g1-api/src/main/java/com/campusdual/cd2024bfs4g1/api/core/service/IStudentBootcampService.java
@@ -1,15 +1,18 @@
 package com.campusdual.cd2024bfs4g1.api.core.service;
 
-import java.util.List;
-import java.util.Map;
-
-import com.ontimize.jee.common.db.AdvancedEntityResult;
 import com.ontimize.jee.common.dto.EntityResult;
 import com.ontimize.jee.common.exceptions.OntimizeJEERuntimeException;
+
+import java.util.List;
+import java.util.Map;
 
 public interface IStudentBootcampService {
 
     EntityResult studentBootcampInsert(Map<String, Object> attrMap) throws OntimizeJEERuntimeException;
 
-    EntityResult studentBootcampUpdate(Map<String, Object> attrMap, Map<String, Object> keyMap) throws OntimizeJEERuntimeException;
+    EntityResult studentBootcampDelete(Map<String, Object> keyMap) throws OntimizeJEERuntimeException;
+
+    EntityResult studentBootcampQuery(Map<String, Object> keyMap, List<String> attrList) throws OntimizeJEERuntimeException;
+
+
 }
