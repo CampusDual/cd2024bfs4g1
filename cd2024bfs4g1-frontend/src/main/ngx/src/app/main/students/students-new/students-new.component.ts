b46a46471826a7661e2d928a07a704155948fffd
Gabriel Alonso Varela <gabrielalonsovarela1@gmail.com>
2024-10-14 11:16:30 +0200

    Front HTML code

---
+++ cd2024bfs4g1-frontend/src/main/ngx/src/app/main/students/students-new/students-new.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-students-new',
+  templateUrl: './students-new.component.html',
+  styleUrls: ['./students-new.component.css']
+})
+export class StudentsNewComponent {
+
+}
--- cd2024bfs4g1-frontend/src/main/ngx/src/app/main/students/students.module.ts
+++ cd2024bfs4g1-frontend/src/main/ngx/src/app/main/students/students.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { OntimizeWebModule } from 'ontimize-web-ngx';
+
 import { StudentsRoutingModule } from './students-routing.module';
+import { StudentsDetailComponent } from './students-detail/students-detail.component';
+import { StudentsNewComponent } from './students-new/students-new.component';
+import { OntimizeWebModule } from 'ontimize-web-ngx';
 import { StudentsTableComponent } from './students-table/students-table.component';
 
 
 @NgModule({
   declarations: [
+    StudentsDetailComponent,
+    StudentsNewComponent
     StudentsTableComponent
   ],
   imports: [
