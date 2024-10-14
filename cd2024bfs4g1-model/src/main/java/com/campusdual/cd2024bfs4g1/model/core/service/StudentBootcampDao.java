d895897c79be2fe0c2613c40442bcf66554aa9fe
David Dominguez Vidal <daviddominguezvidaall@gmail.com>
2024-10-14 15:00:08 +0200

    Backend arreglado (Grupo D)

--- cd2024bfs4g1-model/src/main/java/com/campusdual/cd2024bfs4g1/model/core/service/StudentBootcampDao.java
+++ cd2024bfs4g1-model/src/main/java/com/campusdual/cd2024bfs4g1/model/core/service/StudentBootcampDao.java
@@ -9,27 +9,40 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Map;
 
 @Service("StudentBootcampService")
 @Lazy
 public class StudentBootcampDao implements IStudentBootcampService {
-
     @Autowired
-    private StudentDao studentDao;
-
+    private com.campusdual.cd2024bfs4g1.model.core.dao.StudentBootcampDao studentDao;
     @Autowired
     private DefaultOntimizeDaoHelper daoHelper;
+
+
+
 
     @Override
     public EntityResult studentBootcampInsert(Map<String, Object> attrMap) throws OntimizeJEERuntimeException {
         return this.daoHelper.insert(this.studentDao, attrMap);
+
+    }
+    @Override
+    public EntityResult studentBootcampDelete(Map<String, Object> keyMap) throws OntimizeJEERuntimeException {
+        return this.daoHelper.delete(this.studentDao, keyMap);
     }
 
     @Override
-    public EntityResult studentBootcampUpdate(Map<String, Object> attrMap, Map<String, Object> keyMap) throws OntimizeJEERuntimeException{
-        return this.daoHelper.update(this.studentDao, attrMap, keyMap);
-    };
+    public EntityResult studentBootcampQuery(Map<String, Object> keyMap, List<String> attrList) throws OntimizeJEERuntimeException {
+        return this.daoHelper.query(this.studentDao, keyMap, attrList);    }
+
+    @Override
+    public EntityResult studentBootcampUpdate(Map<String, Object> attrMap, Map<String, Object> keyMap) throws OntimizeJEERuntimeException {
+        return null;
+    }
+
+
 
 
 }
