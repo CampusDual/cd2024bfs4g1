01fc8207bef138aebdeddae15449c9e9f4751762
Fran <franrego333@gmail.com>
2024-10-15 11:52:33 +0200

    Creando front de Bootcamps 2 el select funciona el updadte y delete no

--- cd2024bfs4g1-frontend/src/main/ngx/src/app/main/bootcamps/bootcamps.module.ts
+++ cd2024bfs4g1-frontend/src/main/ngx/src/app/main/bootcamps/bootcamps.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BootcampsRoutingModule } from './bootcamps-routing.module';
-import { BootcampDetailsComponent } from './bootcamp-details/bootcamp-details.component';
+import { BootcampsDetailsComponent } from './bootcamps-details/bootcamps-details.component';
 import { OntimizeWebModule } from 'ontimize-web-ngx';
 import { BootcampListComponent } from './bootcamp-list/bootcamp-list.component';
 import { StudentBootcampAddComponent } from './bootcamp-details/student-bootcamp-add/student-bootcamp-add.component';
