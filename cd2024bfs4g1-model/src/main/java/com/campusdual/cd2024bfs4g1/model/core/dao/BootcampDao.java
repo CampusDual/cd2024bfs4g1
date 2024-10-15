65680a053f1806977a9135568d4b9684c8df8580
Fran <franrego333@gmail.com>
2024-10-15 12:36:26 +0200

    Creando front de Bootcamps 3 el select funciona el updadte y delete +-

--- cd2024bfs4g1-model/src/main/java/com/campusdual/cd2024bfs4g1/model/core/dao/BootcampDao.java
+++ cd2024bfs4g1-model/src/main/java/com/campusdual/cd2024bfs4g1/model/core/dao/BootcampDao.java
@@ -2,6 +2,7 @@ package com.campusdual.cd2024bfs4g1.model.core.dao;
 
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Repository;
+
 import com.ontimize.jee.server.dao.common.ConfigurationFile;
 import com.ontimize.jee.server.dao.jdbc.OntimizeJdbcDaoSupport;
 
@@ -15,4 +16,8 @@ public class BootcampDao extends OntimizeJdbcDaoSupport {
     public static final String ATTR_FINISH_DATE = "fecha_fin";
     public static final String ATTR_STATUS = "status";
 
+    public static final String ATTR_ID = "ID";
+    public static final String ATTR_NAME = "NAME";
+    public static final String ATTR_START_DATE = "START_DATE";
+    public static final String ATTR_END_DATE = "END_DATE";
 }
