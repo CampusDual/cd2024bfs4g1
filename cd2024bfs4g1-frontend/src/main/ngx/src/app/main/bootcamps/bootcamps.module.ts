ca17a776a47bc1db8ef1925a023692a4dc4442e9
Fran <franrego333@gmail.com>
2024-10-15 11:52:33 +0200

    Creando front de Bootcamps 2 el select funciona el updadte y delete no

--- cd2024bfs4g1-frontend/src/main/ngx/src/app/main/bootcamps/bootcamps.module.ts
+++ cd2024bfs4g1-frontend/src/main/ngx/src/app/main/bootcamps/bootcamps.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BootcampsRoutingModule } from './bootcamps-routing.module';
-import { BootcampDetailsComponent } from './bootcamp-details/bootcamp-details.component';
+import { BootcampsDetailsComponent } from './bootcamps-details/bootcamps-details.component';
 import { OntimizeWebModule } from 'ontimize-web-ngx';
 import { BootcampListComponent } from './bootcamp-list/bootcamp-list.component';
 
--- cd2024bfs4g1-frontend/src/main/ngx/src/app/shared/app.services.config.ts
+++ cd2024bfs4g1-frontend/src/main/ngx/src/app/shared/app.services.config.ts
@@ -1,5 +1,3 @@
-//conectar a back en controller en -ws
-
 export const SERVICE_CONFIG: Object = {
   'users': {
     'path': '/users',
