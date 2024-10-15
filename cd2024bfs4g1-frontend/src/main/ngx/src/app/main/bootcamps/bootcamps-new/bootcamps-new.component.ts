93b1c85f77f5f0d1e57a4d4c50433aba66dcf270
Fran <franrego333@gmail.com>
2024-10-15 09:58:59 +0200

    Creando front de Bootcamps 1

---
+++ cd2024bfs4g1-frontend/src/main/ngx/src/app/main/bootcamps/bootcamps-new/bootcamps-new.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bootcamps-new',
+  templateUrl: './bootcamps-new.component.html',
+  styleUrls: ['./bootcamps-new.component.css']
+})
+export class BootcampsNewComponent {
+
+}
