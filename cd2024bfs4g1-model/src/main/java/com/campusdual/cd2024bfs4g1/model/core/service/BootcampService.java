ffb12a8aa8918d71f35497f6707800e5b387319d
eluiui <jesusmiguez@gmail.com>
2024-10-15 11:40:48 +0200

    bootcamp añadido

---
+++ cd2024bfs4g1-model/src/main/java/com/campusdual/cd2024bfs4g1/model/core/service/BootcampService.java
@@ -0,0 +1,46 @@
+package com.campusdual.cd2024bfs4g1.model.core.service;
+
+import com.campusdual.cd2024bfs4g1.api.core.service.IBootcampService;
+import com.campusdual.cd2024bfs4g1.model.core.dao.BootcampDao;
+import com.ontimize.jee.common.dto.EntityResult;
+import com.ontimize.jee.common.exceptions.OntimizeJEERuntimeException;
+import com.ontimize.jee.server.dao.DefaultOntimizeDaoHelper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+
+@Service("BootcampService")
+@Lazy
+public class BootcampService implements IBootcampService {
+
+    @Autowired
+    private BootcampDao bootcampDao;
+    @Autowired
+    private DefaultOntimizeDaoHelper daoHelper;
+
+    @Override
+    public EntityResult bootcampQuery(Map<String, Object> keyMap, List<String> attrList)
+            throws OntimizeJEERuntimeException {
+        return this.daoHelper.query(this.bootcampDao, keyMap, attrList);
+    }
+
+    @Override
+    public EntityResult bootcampInsert(Map<String, Object> attrMap) throws OntimizeJEERuntimeException {
+        return this.daoHelper.insert(this.bootcampDao, attrMap);
+    }
+
+    @Override
+    public EntityResult bootcampUpdate(Map<String, Object> attrMap, Map<String, Object> keyMap)
+            throws OntimizeJEERuntimeException {
+        return this.daoHelper.update(this.bootcampDao, attrMap, keyMap);
+    }
+
+    @Override
+    public EntityResult bootcampDelete(Map<String, Object> keyMap) throws OntimizeJEERuntimeException {
+        return this.daoHelper.delete(this.bootcampDao, keyMap);
+    }
+
+}
