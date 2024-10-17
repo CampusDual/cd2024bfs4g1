9c7f509739a3486986d37f48f48da461074567bf
Fran <franrego333@gmail.com>
2024-10-17 10:06:20 +0200

    Creando Front validadores ready

--- cd2024bfs4g1-frontend/src/main/ngx/src/app/main/bootcamps/bootcamp-new/bootcamp-new.component.ts
+++ cd2024bfs4g1-frontend/src/main/ngx/src/app/main/bootcamps/bootcamp-new/bootcamp-new.component.ts
@@ -10,9 +10,11 @@ import { FormControl, ValidationErrors, ValidatorFn } from '@angular/forms';
 export class BootcampNewComponent {
 
   validatorsArray: ValidatorFn[] = [];
+  validatorsArray1: ValidatorFn[] = [];
 
   constructor(private router: Router) {
     this.validatorsArray.push(this.dateValidator);
+    this.validatorsArray1.push(this.dateValidator1);
   }
 
   volver(e) {
@@ -25,6 +27,21 @@ export class BootcampNewComponent {
     if (control && control.parent && control.value) {
       let enddate = control.value.valueOf();
       let startdate = control.parent.value.start_date;
+
+      if (enddate && startdate && enddate < startdate) {
+        result['wrongendate'] = true;
+      }
+    }
+
+    return result;
+  }
+
+  dateValidator1(control: FormControl): ValidationErrors {
+    let result = {};
+
+    if (control && control.parent && control.value) {
+      let enddate = control.parent.value.end_date;
+      let startdate = control.value.valueOf();
 
       if (enddate && startdate && enddate < startdate) {
         result['wrongendate'] = true;
