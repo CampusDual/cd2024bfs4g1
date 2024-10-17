ee8e5f9fa581642ba50103f158073023caebd4f4
Fran <franrego333@gmail.com>
2024-10-17 10:06:20 +0200

    Creando Front validadores ready

--- cd2024bfs4g1-frontend/src/main/ngx/src/app/main/bootcamps/bootcamp-details/bootcamp-details.component.ts
+++ cd2024bfs4g1-frontend/src/main/ngx/src/app/main/bootcamps/bootcamp-details/bootcamp-details.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, ValidationErrors, ValidatorFn } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -9,12 +10,45 @@ import { Router } from '@angular/router';
 })
 export class BootcampDetailsComponent {
 
-  constructor(private router:Router){
+  validatorsArray: ValidatorFn[] = [];
+  validatorsArray1: ValidatorFn[] = [];
 
+  constructor(private router: Router) {
+    this.validatorsArray.push(this.dateValidator);
+    this.validatorsArray1.push(this.dateValidator1);
   }
 
-volver(e :any){
-this.router.navigate(['./main/bootcamps']);
+  volver(e) {
+    this.router.navigate(['./main/bootcamps']);
+  }
 
+  dateValidator(control: FormControl): ValidationErrors {
+    let result = {};
+
+    if (control && control.parent && control.value) {
+      let enddate = control.value.valueOf();
+      let startdate = control.parent.value.start_date;
+
+      if (enddate && startdate && enddate < startdate) {
+        result['wrongendate'] = true;
+      }
     }
+
+    return result;
+  }
+
+  dateValidator1(control: FormControl): ValidationErrors {
+    let result = {};
+
+    if (control && control.parent && control.value) {
+      let enddate = control.parent.value.end_date;
+      let startdate = control.value.valueOf();
+
+      if (enddate && startdate && enddate < startdate) {
+        result['wrongendate'] = true;
+      }
+    }
+
+    return result;
+  }
 }
