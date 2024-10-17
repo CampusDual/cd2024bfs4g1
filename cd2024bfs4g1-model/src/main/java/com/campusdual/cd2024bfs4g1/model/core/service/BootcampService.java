9380c5fbe14b7fc1a1cfdbf8bf574a4b15f8c8ec
DeNgo <ruannicolas_@hotmail.com>
2024-10-17 10:44:14 +0200

    Validacion Back fechas

--- cd2024bfs4g1-model/src/main/java/com/campusdual/cd2024bfs4g1/model/core/service/BootcampService.java
+++ cd2024bfs4g1-model/src/main/java/com/campusdual/cd2024bfs4g1/model/core/service/BootcampService.java
@@ -3,12 +3,16 @@ package com.campusdual.cd2024bfs4g1.model.core.service;
 import com.campusdual.cd2024bfs4g1.api.core.service.IBootcampService;
 import com.campusdual.cd2024bfs4g1.model.core.dao.BootcampDao;
 import com.ontimize.jee.common.dto.EntityResult;
+import com.ontimize.jee.common.dto.EntityResultMapImpl;
 import com.ontimize.jee.common.exceptions.OntimizeJEERuntimeException;
 import com.ontimize.jee.server.dao.DefaultOntimizeDaoHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
@@ -29,6 +33,25 @@ public class BootcampService implements IBootcampService {
 
     @Override
     public EntityResult bootcampInsert(Map<String, Object> attrMap) throws OntimizeJEERuntimeException {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");  // Asegúrate que este formato coincide con el formato de las fechas
+
+        try {
+            Date startDate = dateFormat.parse((String) attrMap.get(BootcampDao.ATTR_START_DATE));
+            Date finishDate = dateFormat.parse((String) attrMap.get(BootcampDao.ATTR_FINISH_DATE));
+
+            if (finishDate.before(startDate)) {
+                EntityResult error = new EntityResultMapImpl();
+                error.setCode(EntityResult.OPERATION_WRONG);
+                error.setMessage("END_DATE_MORE_THAN_INIT_DATE");
+                return error;
+            }
+        } catch (ParseException e) {
+            EntityResult error = new EntityResultMapImpl();
+            error.setCode(EntityResult.OPERATION_WRONG);
+            error.setMessage("INVALID_DATE_FORMAT");
+            return error;
+        }
+
         return this.daoHelper.insert(this.bootcampDao, attrMap);
     }
 
