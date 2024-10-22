18b255b26b6bbc43fffe9a3366cb8c224c91e4b6
marcos.padin@outlook.es <you@example.com>
2024-10-22 14:41:18 +0200

    Rama 10.

--- cd2024bfs4g1-frontend/src/main/ngx/src/app/main/students/students-table/students-table.component.ts
+++ cd2024bfs4g1-frontend/src/main/ngx/src/app/main/students/students-table/students-table.component.ts
@@ -1,6 +1,9 @@
 import { Component} from '@angular/core';
 import { FilterExpressionUtils, Expression, OFormComponent } from 'ontimize-web-ngx';
 
+import{FilterExpressionUtils, Expression} from 'ontimize-web-ngx';
+
+
 @Component({
   selector: 'app-students-table',
   templateUrl: './students-table.component.html',
@@ -35,5 +38,3 @@ export class StudentsTableComponent {
     }
   }
 }
-
-
