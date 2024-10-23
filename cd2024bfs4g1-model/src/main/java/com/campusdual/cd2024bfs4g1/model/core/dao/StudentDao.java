f022d771f016a6872fd79cdce7172e37a768ce0c
Velasco <quarantine.sleep480@passinbox.com>
2024-10-23 12:23:00 +0200

    fix: style fixes

--- cd2024bfs4g1-model/src/main/java/com/campusdual/cd2024bfs4g1/model/core/dao/StudentDao.java
+++ cd2024bfs4g1-model/src/main/java/com/campusdual/cd2024bfs4g1/model/core/dao/StudentDao.java
@@ -11,21 +11,21 @@ import org.springframework.stereotype.Repository;
         configurationFile = "dao/StudentDao.xml",
         configurationFilePlaceholder = "dao/placeholders.properties")
 public class StudentDao extends OntimizeJdbcDaoSupport {
-        public static final String STU_ID   = "id";
-        public static final String NAME     = "name";
-        public static final String SURNAME1 = "surname1";
+        public static final String STU_ID            = "id";
+        public static final String NAME              = "name";
+        public static final String SURNAME1          = "surname1";
         public static final String PERSONAL_EMAIL    = "email";
-        public static final String DNI = "dni";
-        public static final String SURNAME2 = "surname2";
-        public static final String BIRTH_DATE = "birth_date";
-        public static final String PHONE = "phone";
+        public static final String DNI               = "dni";
+        public static final String SURNAME2          = "surname2";
+        public static final String BIRTH_DATE        = "birth_date";
+        public static final String PHONE             = "phone";
         public static final String EMPLOYMENT_STATUS = "employment_status";
-        public static final String CAMPUS_EMAIL = "campus_email";
-        public static final String FCT_SCHOOL = "fct_school";
-        public static final String FCT_START = "fct_start";
-        public static final String FCT_END = "fct_end";
-        public static final String TUTOR = "tutor";
-        public static final String UDEMY = "udemy";
-        public static final String GITHUB_USER = "github_user";
-        public static final String NOTES = "notes";
+        public static final String CAMPUS_EMAIL      = "campus_email";
+        public static final String FCT_SCHOOL        = "fct_school";
+        public static final String FCT_START         = "fct_start";
+        public static final String FCT_END           = "fct_end";
+        public static final String TUTOR             = "tutor";
+        public static final String UDEMY             = "udemy";
+        public static final String GITHUB_USER       = "github_user";
+        public static final String NOTES             = "notes";
 }
