8caec3d17ccfdd3f6d8240c46577852b66aee811
JoseMRLC <josemriveiroticbgmail.com>
2024-10-24 10:37:57 +0200

    date validators v1

--- cd2024bfs4g1-frontend/src/main/ngx/src/app/main/students/students-new/students-new.component.ts
+++ cd2024bfs4g1-frontend/src/main/ngx/src/app/main/students/students-new/students-new.component.ts
@@ -1,5 +1,7 @@
 import { Component, ViewChild } from '@angular/core';
 import { OFormComponent } from 'ontimize-web-ngx';
+import { ODateInputComponent } from 'ontimize-web-ngx';
+import { FormControl, ValidationErrors, ValidatorFn } from '@angular/forms';
 
 @Component({
   selector: 'app-students-new',
@@ -10,18 +12,40 @@ export class StudentsNewComponent {
 
   @ViewChild('studentsform') protected formStudents: OFormComponent;
  
+  validatorsArray: ValidatorFn[] = [];
+  validatorsArray1: ValidatorFn[] = [];
+  
+  
+  constructor() {
+    this.validatorsArray.push(this.dateValidator);
+  }
+
+
+
   insertStudent() {
     this.formStudents.insert();
   }
   
-    getValueBooleanType(type) {
-      switch (type) {
-        case 'number':
-          return 1;
-        case 'boolean':
-          return false;
-        case 'string':
-          return 'Y'
+  dateValidator(control: FormControl): ValidationErrors {
+    let result = {};
+
+    if (control && control.parent && control.value) {
+      let enddate = control.value.valueOf();
+      let startdate = control.parent.value.start_date;
+
+      if (enddate && startdate && enddate < startdate) {
+        result['wrongendate'] = true;
       }
     }
+
+    return result;
+  }
+
+
+  throwChange(enddate: ODateInputComponent) {
+    enddate.getControl().updateValueAndValidity();
+  }
+  throwChange2(startdate: ODateInputComponent) {
+    startdate.getControl().updateValueAndValidity();
+  }
 }
--- cd2024bfs4g1-model/src/main/java/com/campusdual/cd2024bfs4g1/model/core/service/StudentService.java
+++ cd2024bfs4g1-model/src/main/java/com/campusdual/cd2024bfs4g1/model/core/service/StudentService.java
@@ -1,9 +1,12 @@
 package com.campusdual.cd2024bfs4g1.model.core.service;
 
+import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
 import com.campusdual.cd2024bfs4g1.api.core.service.IStudentService;
+import com.campusdual.cd2024bfs4g1.model.core.dao.BootcampDao;
 import com.campusdual.cd2024bfs4g1.model.core.dao.StudentDao;
 import com.ontimize.jee.common.dto.EntityResult;
 import com.ontimize.jee.common.dto.EntityResultMapImpl;
@@ -33,16 +36,50 @@ public class StudentService implements IStudentService {
 
 	@Override
 	public EntityResult studentInsert(Map<String, Object> attrMap) throws OntimizeJEERuntimeException {
+		Date startDate = (Date) attrMap.get(studentDao.FCT_START);
+		Date finishDate = (Date) attrMap.get(studentDao.FCT_END);
+
+		if (finishDate.before(startDate)) {
+			EntityResult error = new EntityResultMapImpl();
+			error.setCode(EntityResult.OPERATION_WRONG);
+			error.setMessage("END_DATE_MORE_THAN_INIT_DATE");
+			return error;
+		}
+
 		return this.daoHelper.insert(this.studentDao, attrMap);
 	}
 
 	@Override
 	public EntityResult studentUpdate(Map<String, Object> attrMap, Map<String, Object> keyMap) throws OntimizeJEERuntimeException {
+
+
+			EntityResult query = this.daoHelper.query(this.studentDao, keyMap,
+					Arrays.asList(studentDao.FCT_START, studentDao.FCT_END));
+
+			Map<String, Object> mapResult = query.getRecordValues(0);
+			Date currentStartDate = (Date) mapResult.get(studentDao.FCT_START);
+			Date currentFinishDate = (Date) mapResult.get(studentDao.FCT_END);
+
+			Date newStartDate = (Date) attrMap.getOrDefault(studentDao.FCT_START, currentStartDate);
+			Date newFinishDate = (Date) attrMap.getOrDefault(studentDao.FCT_END, currentFinishDate);
+
+			if (newFinishDate.before(newStartDate)) {
+				return createErrorResult("END_DATE_MORE_THAN_INIT_DATE");
+			}
+
+
+
 		return this.daoHelper.update(this.studentDao, attrMap, keyMap);
 	}
 
 	@Override
 	public EntityResult studentDelete(Map<String, Object> keyMap) throws OntimizeJEERuntimeException {
 		return this.daoHelper.delete(this.studentDao, keyMap);
+	}
+	private EntityResult createErrorResult(String message) {
+		EntityResult error = new EntityResultMapImpl();
+		error.setCode(EntityResult.OPERATION_WRONG);
+		error.setMessage(message);
+		return error;
 	}
 }
