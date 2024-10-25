e140734cd3acce9f5ee13effadef96a71afbbc52
David Dominguez Vidal <daviddominguezvidaall@gmail.com>
2024-10-25 14:22:26 +0200

    calendario terminado

--- cd2024bfs4g1-frontend/src/main/ngx/src/app/main/home/home.module.ts
+++ cd2024bfs4g1-frontend/src/main/ngx/src/app/main/home/home.module.ts
@@ -4,12 +4,16 @@ import { OntimizeWebModule } from 'ontimize-web-ngx';
 import { SharedModule } from '../../shared/shared.module';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 
 @NgModule({
   imports: [
     SharedModule,
     OntimizeWebModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
   ],
   declarations: [
     HomeComponent
