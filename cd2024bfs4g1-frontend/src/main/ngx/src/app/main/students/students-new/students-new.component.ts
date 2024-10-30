435b4994b29e46b76d7fb3a11f289d0b9efcc4b5
Fran <franrego333@gmail.com>
2024-10-30 12:37:12 +0100

    Comit rapidillo

--- cd2024bfs4g1-frontend/src/main/ngx/src/app/main/students/students-new/students-new.component.ts
+++ cd2024bfs4g1-frontend/src/main/ngx/src/app/main/students/students-new/students-new.component.ts
@@ -52,5 +52,8 @@ export class StudentsNewComponent {
   throwChange2(startdate: ODateInputComponent) {
     startdate.getControl().updateValueAndValidity();
   }
+  public getValue() {
+    return '+34 986350677';
+  }
 }
 
