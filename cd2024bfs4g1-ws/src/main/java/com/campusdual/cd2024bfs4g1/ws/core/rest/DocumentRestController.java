13d94302422e58ec8ce5a3d7d185f3c1b8b66140
eluiui <jesusmiguez@gmail.com>
2024-10-30 21:53:13 +0100

    encamidados al triunfo 2(añadi una cosita y ya no tira el error method not alowed pero todabia no hace nada)

--- cd2024bfs4g1-ws/src/main/java/com/campusdual/cd2024bfs4g1/ws/core/rest/DocumentRestController.java
+++ cd2024bfs4g1-ws/src/main/java/com/campusdual/cd2024bfs4g1/ws/core/rest/DocumentRestController.java
@@ -11,7 +11,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
@@ -21,6 +23,8 @@ import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
 
+@RestController
+@RequestMapping("/documents")
 public class DocumentRestController extends ORestController<IDocumentService> {
     public static final String STATUS = "status";
     public static final String NAME = "name";
