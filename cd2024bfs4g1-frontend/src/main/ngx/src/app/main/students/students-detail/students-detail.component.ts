bfd627ecfb8423294c1a40818a98ab6cd9db78b4
David Dominguez Vidal <daviddominguezvidaall@gmail.com>
2024-11-05 12:34:58 +0100

    se muestra archivos con path en la tabla

--- cd2024bfs4g1-frontend/src/main/ngx/src/app/main/students/students-detail/students-detail.component.ts
+++ cd2024bfs4g1-frontend/src/main/ngx/src/app/main/students/students-detail/students-detail.component.ts
@@ -1,5 +1,5 @@
-import { Component, ViewChild } from '@angular/core';
-import { OFormComponent, OTextInputComponent } from 'ontimize-web-ngx';
+import { Component, Injector, ViewChild } from '@angular/core';
+import { OFormComponent, OntimizeService, OTextInputComponent } from 'ontimize-web-ngx';
 import { ODateInputComponent } from 'ontimize-web-ngx';
 import { FormControl, ValidationErrors, ValidatorFn } from '@angular/forms';
 import { Router } from '@angular/router';
@@ -16,6 +16,8 @@ export class StudentsDetailComponent {
   validatorsArray: ValidatorFn[] = [];
   validatorsArray1: ValidatorFn[] = [];
   dataArray = spainComunitys.map(comunity => ({ key: comunity, value: comunity }));
+  protected service: OntimizeService;
+  protected injector: Injector;
 
   // Valor predeterminado (opcional)
   valueSimple = "Madrid"; // Elige el valor que deseas predeterminar
@@ -23,6 +25,13 @@ export class StudentsDetailComponent {
 
   constructor(private router: Router, public location: Location) {
     this.validatorsArray.push(this.dateValidator);
+    this.service = this.injector.get(OntimizeService);
+    this.configureService();
+  }
+
+  protected configureService() {
+    const conf = this.service.getDefaultServiceConfiguration('documents');
+    this.service.configureService(conf);
   }
 
   dateValidator(control: FormControl): ValidationErrors {
@@ -94,5 +103,44 @@ export class StudentsDetailComponent {
   showError(event: any) {
     console.log(event);
   }
+/*
+  // Método para manejar el evento de clic en la acción
+  actionClick(event) {
+    // Se realiza una consulta al servicio personalDocumentService para obtener los datos del archivo correspondiente al evento de clic.
+    this.service.query({ id: event.id }, ['name', 'base64'], 'myPersonalFilesContent').subscribe(res => {
+      if (res.data && res.data.length) {
+        // Si se encuentran datos, se extrae el nombre del archivo y el contenido en base64.
+        let filename = res.data[0].name;
+        let base64 = res.data[0].base64;
+        // Se crea un enlace temporal para descargar el archivo.
+        const src = data:text/csv;base64,${base64};
+        const link = document.createElement("a");
+        link.href = src;
+        link.download = filename;
+        link.click();
+        link.remove();
+      }
+    });
+
+  }
+
+  // Método para manejar la ejecución de la acción desde el menú contextual
+  onExecute(event: any) {
+    // Se realiza una consulta al servicio personalDocumentService para obtener los datos del archivo correspondiente a la acción ejecutada.
+    this.service.query({ id: event.id }, ['name', 'base64'], 'myPersonalFilesContent').subscribe(res => {
+      if (res.data && res.data.length) {
+        // Si se encuentran datos, se extrae el nombre del archivo y el contenido en base64.
+        let filename = res.data[0].name;
+        let base64 = res.data[0].base64;
+        // Se crea un enlace temporal para descargar el archivo.
+        const src = data:text/csv;base64,${base64};
+        const link = document.createElement("a");
+        link.href = src;
+        link.download = filename;
+        link.click();
+        link.remove();
+      }
+    });
+  }*/
 
 }
