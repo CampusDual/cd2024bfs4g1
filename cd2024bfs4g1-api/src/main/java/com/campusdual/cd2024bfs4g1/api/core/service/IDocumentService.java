1e52032fda2b90b58d00eb69c1947cd208c085ae
David Dominguez Vidal <daviddominguezvidaall@gmail.com>
2024-11-05 10:52:52 +0100

    back del spike "preparado/pasado a limpio"

---
+++ cd2024bfs4g1-api/src/main/java/com/campusdual/cd2024bfs4g1/api/core/service/IDocumentService.java
@@ -0,0 +1,13 @@
+package com.campusdual.cd2024bfs4g1.api.core.service;
+import com.ontimize.jee.common.dto.EntityResult;
+import java.util.List;
+import java.util.Map;
+
+public interface IDocumentService {
+
+    public EntityResult personalFilesQuery(Map<String, Object> keyMap, List<String> attrList);//throws OntimizeJEERuntimeException;
+
+    public EntityResult personalFileInsert(Map<String, Object> attrMap);//throws OntimizeJEERuntimeException;
+
+    EntityResult personalFilesDelete(Map<String, Object> keyMap);//throws OntimizeJEERuntimeException;
+}
