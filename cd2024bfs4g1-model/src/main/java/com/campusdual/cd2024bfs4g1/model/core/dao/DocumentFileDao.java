bf0a46d774a3fca9b9f81dd15d0f134f34025d56
eluiui <jesusmiguez@gmail.com>
2024-11-06 14:57:07 +0100

    salimos triunfales con todo hecho pero tiene casos de uso

--- cd2024bfs4g1-model/src/main/java/com/campusdual/cd2024bfs4g1/model/core/dao/DocumentFileDao.java
+++ cd2024bfs4g1-model/src/main/java/com/campusdual/cd2024bfs4g1/model/core/dao/DocumentFileDao.java
@@ -10,7 +10,6 @@ import org.springframework.stereotype.Repository;
 @ConfigurationFile(configurationFile = "dao/DocumentFileDao.xml", configurationFilePlaceholder = "dao/placeholders.properties")
 public class DocumentFileDao extends OntimizeJdbcDaoSupport {
     public static final String ATTR_ID = "id";
-    public static final String ATTR_ID_STUDENT = "student_id";
     public static final String ATTR_NAME = "name";
     public static final String ATTR_PATH = "file_path";
     public static final String ATTR_BASE64 = "base64";
