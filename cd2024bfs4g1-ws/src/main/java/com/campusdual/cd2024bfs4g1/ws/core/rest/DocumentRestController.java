7fab6979251621fdfa1ae22bfbd0dd20b05db252
eluiui <jesusmiguez@gmail.com>
2024-11-06 14:57:07 +0100

    salimos triunfales con todo hecho pero tiene casos de uso

--- cd2024bfs4g1-ws/src/main/java/com/campusdual/cd2024bfs4g1/ws/core/rest/DocumentRestController.java
+++ cd2024bfs4g1-ws/src/main/java/com/campusdual/cd2024bfs4g1/ws/core/rest/DocumentRestController.java
@@ -2,6 +2,7 @@ package com.campusdual.cd2024bfs4g1.ws.core.rest;
 
 import com.campusdual.cd2024bfs4g1.model.core.dao.DocumentFileDao;
 import com.campusdual.cd2024bfs4g1.api.core.service.IDocumentService;
+import com.campusdual.cd2024bfs4g1.model.core.dao.StudentDocumentDao;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ontimize.jee.common.dto.EntityResult;
@@ -45,6 +46,7 @@ public class DocumentRestController extends ORestController<IDocumentService> {
     public ResponseEntity upload(@RequestParam("name") String[] names, @RequestParam("file") MultipartFile[] files, @RequestParam(name = "data", required = false) String data) {
 
         HashMap<String, Object> extraData = new HashMap<>();
+        HashMap<String, Object> extraData2 = new HashMap<>();
         if (data != null) {
             try {
                 extraData = new ObjectMapper().readValue(data, HashMap.class);
@@ -54,8 +56,8 @@ public class DocumentRestController extends ORestController<IDocumentService> {
         }
         Number student_id = null;
         EntityResult result = new EntityResultMapImpl();
-        if (extraData.get(DocumentFileDao.ATTR_ID_STUDENT) instanceof Number) {
-            student_id = (Number) extraData.get(DocumentFileDao.ATTR_ID_STUDENT);
+        if (extraData.get(StudentDocumentDao.ATTR_ID_STUDENT) instanceof Number) {
+            student_id = (Number) extraData.get(StudentDocumentDao.ATTR_ID_STUDENT);
 
             String directory = path + student_id;
             try {
@@ -73,11 +75,19 @@ public class DocumentRestController extends ORestController<IDocumentService> {
                         fileResult.put(STATUS, "ALREADY_EXIST");
                     } else {
                         file.transferTo(newFile);
+
                         Map<String, Object> attrMap = new HashMap();
-                        attrMap.put(DocumentFileDao.ATTR_ID_STUDENT, student_id);
                         attrMap.put(DocumentFileDao.ATTR_NAME, file.getOriginalFilename());
                         attrMap.put(DocumentFileDao.ATTR_PATH, filePath);
                         EntityResult fileInsert = documentsrv.personalFileInsert(attrMap);
+                        Number id = null;
+                        if (fileInsert.get(DocumentFileDao.ATTR_ID) instanceof Number) {
+                            id = (Number) fileInsert.get(DocumentFileDao.ATTR_ID);
+                            Map<String, Object> attrMap2 = new HashMap();
+                            attrMap2.put(StudentDocumentDao.ATTR_ID_STUDENT, student_id);
+                            attrMap2.put(StudentDocumentDao.ATTR_ID_DOCUMENT, id);
+                            documentsrv.studentdocumentInsert(attrMap2);
+                        }
                         if (fileInsert.isWrong()) {
                             fileResult.put(NAME, file.getOriginalFilename());
                             fileResult.put(STATUS, "ERROR_ON_INSERT");
@@ -94,6 +104,6 @@ public class DocumentRestController extends ORestController<IDocumentService> {
             }
         }
 
-         return new ResponseEntity<EntityResult>(result, HttpStatus.OK);
+        return new ResponseEntity<EntityResult>(result, HttpStatus.OK);
     }
 }
