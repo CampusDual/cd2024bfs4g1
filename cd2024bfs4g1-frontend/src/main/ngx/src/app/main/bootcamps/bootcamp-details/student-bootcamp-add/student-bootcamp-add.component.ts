fcb96a4ee5d886243ed6c03e20c185ea911bf1bb
Fran <franrego333@gmail.com>
2024-11-07 10:21:36 +0100

    Probando cosas del date range

--- cd2024bfs4g1-frontend/src/main/ngx/src/app/main/bootcamps/bootcamp-details/student-bootcamp-add/student-bootcamp-add.component.ts
+++ cd2024bfs4g1-frontend/src/main/ngx/src/app/main/bootcamps/bootcamp-details/student-bootcamp-add/student-bootcamp-add.component.ts
@@ -1,14 +1,26 @@
 import { Component, ViewChild } from '@angular/core';
-import { OFormComponent } from 'ontimize-web-ngx';
+import moment from 'moment';
+import { ODateInputComponent, OFormComponent } from 'ontimize-web-ngx';
 
 @Component({
   selector: 'app-student-bootcamp-add',
   templateUrl: './student-bootcamp-add.component.html',
   styleUrls: ['./student-bootcamp-add.component.css']
 })
+
 export class StudentBootcampAddComponent {
 
+
   @ViewChild('studentBootcampForm') studentBootcampForm:OFormComponent;
+  @ViewChild("startdate") startDateInput: ODateInputComponent;
+  @ViewChild("enddate") endDateInput: ODateInputComponent;
+
+
+  public selected = {};
+  $event: any;
+
+
+ 
 
   addStudentBootcamp() {
       this.studentBootcampForm.insert();
@@ -17,5 +29,17 @@ export class StudentBootcampAddComponent {
 
   navigateBack() {
     window.history.back();
+  }
+
+  candidate(){
+    this.studentBootcampForm.setFieldValue("status","Candidato")
+  }
+  getDateBootcamp(){
+
+    this.selected = {
+      startDate: this.startDateInput,
+      endDate: this.endDateInput
+    };
+    return this.selected
   }
 }
