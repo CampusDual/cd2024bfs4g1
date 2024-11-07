34c220384ea682fd5f3796b88bcec7cbd4bd6e00
Gabriel Alonso Varela <gabrielalonsovarela1@gmail.com>
2024-11-07 11:58:06 +0100

    Tutor Detail Changes

--- cd2024bfs4g1-frontend/src/main/ngx/src/app/main/tutors/tutors-routing.module.ts
+++ cd2024bfs4g1-frontend/src/main/ngx/src/app/main/tutors/tutors-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { TutorsNewComponent } from './tutors-new/tutors-new.component';
 import { TutorsDetailComponent } from './tutors-detail/tutors-detail.component';
 import { TutorsTableComponent } from './tutors-table/tutors-table.component';
+import { BootcampTutorAddComponent } from './tutors-detail/bootcamp-tutor-add/bootcamp-tutor-add.component';
 
 const routes: Routes = [
   { path:'', pathMatch:'full', component: TutorsTableComponent},
   { path:"new", component:TutorsNewComponent},
-  { path:":id", component:TutorsDetailComponent}
+  { path:":id", component:TutorsDetailComponent},
+  { path:":tutor_id/new", component:BootcampTutorAddComponent}
 ];
 
 @NgModule({
--- cd2024bfs4g1-frontend/src/main/ngx/src/app/main/tutors/tutors.module.ts
+++ cd2024bfs4g1-frontend/src/main/ngx/src/app/main/tutors/tutors.module.ts
@@ -6,13 +6,15 @@ import { TutorsDetailComponent } from './tutors-detail/tutors-detail.component';
 import { TutorsNewComponent } from './tutors-new/tutors-new.component';
 import { TutorsTableComponent } from './tutors-table/tutors-table.component';
 import { OntimizeWebModule } from 'ontimize-web-ngx';
+import { BootcampTutorAddComponent } from './tutors-detail/bootcamp-tutor-add/bootcamp-tutor-add.component';
 
 
 @NgModule({
   declarations: [
     TutorsDetailComponent,
     TutorsNewComponent,
-    TutorsTableComponent
+    TutorsTableComponent,
+    BootcampTutorAddComponent
   ],
   imports: [
     CommonModule,
