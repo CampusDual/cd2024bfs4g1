74d17b62f8a60ea4416627314176c92692792b23
marcos.padin@outlook.es <you@example.com>
2024-11-08 12:52:31 +0100

    ocultar menu roles crear formulario vista usuario actualizar contraseña y usuario, relación con roles de la tabla de sql actualización de estilo y traducciones

--- cd2024bfs4g1-model/src/main/java/com/campusdual/cd2024bfs4g1/model/core/service/StudentService.java
+++ cd2024bfs4g1-model/src/main/java/com/campusdual/cd2024bfs4g1/model/core/service/StudentService.java
@@ -37,6 +37,12 @@ public class StudentService implements IStudentService {
 	@Autowired
 	private IUserAndRoleService userAndRoleService;
 
+	@Autowired
+	private UserRoleDao userRoleDao;
+
+	@Autowired
+	private RoleDao roleDao;
+
 	@Override
 	public EntityResult studentQuery(Map<String, Object> keysMap, List<String> attributes) throws OntimizeJEERuntimeException {
 		return this.daoHelper.query(this.studentDao, keysMap, attributes);
@@ -54,7 +60,53 @@ if((attrMap.get(studentDao.FCT_START) != null) && attrMap.get(studentDao.FCT_END
 		return error;
 	}
 }
-		return this.daoHelper.insert(this.studentDao, attrMap);
+		String usrLogin = (String) attrMap.remove(UserDao.LOGIN);
+		String usrPassword = (String) attrMap.remove(UserDao.PASSWORD);
+		//insertar datos alumno
+		EntityResult insertStudent = this.daoHelper.insert(this.studentDao, attrMap);
+		if(insertStudent.isWrong()){
+			return insertStudent;
+		}
+		//insertar en caso de null
+		if(usrLogin == null && usrPassword == null){
+			return insertStudent;
+		}
+		//crear mapa de atributos para insertar usuario
+		Map<String, Object> userAttrMap = new HashMap<>();
+		if(usrLogin != null){
+			userAttrMap.put(UserDao.LOGIN, usrLogin);
+		}
+		if(usrPassword != null){
+			userAttrMap.put(UserDao.PASSWORD, usrPassword);
+		}
+
+		//insertamos usuario
+		EntityResult insertUser = userAndRoleService.userInsert(userAttrMap);
+		if(insertUser.isWrong()){
+			return insertUser;
+		}
+
+		//obtener id del usuario insertado
+		Integer userId = (Integer) insertUser.get(UserDao.USR_ID);
+
+		//asignar rol de estudiante al usuario
+		EntityResult assignRoleResult = assignStudentRole(userId);
+		if(assignRoleResult.isWrong()){
+			return assignRoleResult;
+		}
+
+		//actualizar tabla estudiantes con id usuario
+		Map<String, Object> attrStudentUser = new HashMap<>();
+		attrStudentUser.put(StudentDao.USER_ID, userId);
+		Map<String, Object> keyMap = new HashMap<>();
+		keyMap.put(StudentDao.STU_ID, insertStudent.get(StudentDao.STU_ID));
+
+		EntityResult updateStudentWithUser = this.daoHelper.update(this.studentDao, attrStudentUser, keyMap);
+		if(updateStudentWithUser.isWrong()){
+			return updateStudentWithUser;
+		}
+		return insertStudent;
+
 	}
 
 	@Override
@@ -70,7 +122,6 @@ if((attrMap.get(studentDao.FCT_START) != null) && attrMap.get(studentDao.FCT_END
 
 			Date currentStartDate = (Date) mapResult.get(studentDao.FCT_START);
 			Date currentFinishDate = (Date) mapResult.get(studentDao.FCT_END);
-
 			Date newStartDate = (Date) attrMap.getOrDefault(studentDao.FCT_START, currentStartDate);
 			Date newFinishDate = (Date) attrMap.getOrDefault(studentDao.FCT_END, currentFinishDate);
 
@@ -109,10 +160,17 @@ if((attrMap.get(studentDao.FCT_START) != null) && attrMap.get(studentDao.FCT_END
 			Map<String, Object> userKeyMap = new HashMap<>();
 			userKeyMap.put(UserDao.USR_ID, userId);
 
-			return userAndRoleService.userUpdate(userAttrMap, userKeyMap);
+			EntityResult userUpdateResult = userAndRoleService.userUpdate(userAttrMap,userKeyMap);
+			if(userUpdateResult.isWrong()){
+				return userUpdateResult;
+			}
 
-
-
+			//asignar rol de estudiante al user
+			EntityResult assignRoleResult = assignStudentRole(userId);
+			if(assignRoleResult.isWrong()){
+				return assignRoleResult;
+			}
+			return userUpdateResult;
 		}else{
 			//SI NO SE ENCUENTRA SE INSERTA
 			Map<String, Object> userAttrMap = new HashMap<>();
@@ -129,9 +187,23 @@ if((attrMap.get(studentDao.FCT_START) != null) && attrMap.get(studentDao.FCT_END
 				return insertUser;
 			}
 			userId = (Integer) insertUser.get(UserDao.USR_ID);
+
+			//actualizar la tabla estudiantes con id de usuario
 			Map<String, Object> attrStudentUser = new HashMap<>();
 			attrStudentUser.put(StudentDao.USER_ID, userId);
-			return this.daoHelper.update(this.studentDao, attrStudentUser, keyMap);
+
+			EntityResult updateStudentWithUser = this.daoHelper.update(this.studentDao, attrStudentUser, keyMap);
+			if(updateStudentWithUser.isWrong()){
+				return updateStudentWithUser;
+			}
+
+			//asignar rol de estudiante al usuario
+			EntityResult assignRoleResult = assignStudentRole(userId);
+			if(assignRoleResult.isWrong()){
+				return assignRoleResult;
+			}
+
+			return updateStudentWithUser;
 
 		}
 
@@ -155,6 +227,50 @@ if((attrMap.get(studentDao.FCT_START) != null) && attrMap.get(studentDao.FCT_END
 		}
 
 	}
+
+	private EntityResult assignStudentRole(Integer userId){
+		//ir a tabla roles para conseguir rol de estudiante
+		Map<String, Object> roleFilter = new HashMap<>();
+		roleFilter.put(RoleDao.ROL_NAME, "student");
+		List<String> roleColumns = Arrays.asList(RoleDao.ROL_ID);
+
+		EntityResult roleResult = this.daoHelper.query(this.roleDao, roleFilter, roleColumns);
+		if(roleResult.isWrong() || roleResult.calculateRecordNumber() == 0){
+			return createErrorResult("STUDENT_ROLE_NOT_FOUND");
+		}
+
+		Integer roleId = (Integer) roleResult.getRecordValues(0).get(RoleDao.ROL_ID);
+
+		//relacion con usr_user_rol
+		Map<String, Object> userRoleMap = new HashMap<>();
+		userRoleMap.put(UserDao.USR_ID, userId);
+		userRoleMap.put(RoleDao.ROL_ID, roleId);
+
+		return this.daoHelper.insert(this.userRoleDao, userRoleMap);
+	}
+
+
+
+	/*//NUEVO DESDE AQUI
+	@Override
+	public Map<String, Object> getStudentById(String userLogin) {
+		// Crear filtro
+		Map<String, Object> filter = new HashMap<>();
+		filter.put("usr_login", userLogin);
+		// Usar `userId` como clave
+		// Crear lista de columnas que quieres recuperar
+		List<String> columns = List.of(StudentDao.STU_ID, StudentDao.NAME, StudentDao.SURNAME1, StudentDao.SURNAME2,
+				StudentDao.DNI, StudentDao.PHONE, StudentDao.EMPLOYMENT_STATUS, StudentDao.LOCATION,
+				StudentDao.BIRTH_DATE, StudentDao.PERSONAL_EMAIL, StudentDao.CAMPUS_EMAIL, StudentDao.FCT_SCHOOL,
+				StudentDao.TUTOR, StudentDao.FCT_START, StudentDao.FCT_END, StudentDao.UDEMY, StudentDao.GITHUB_USER);
+		// Realizar consulta en la base de datos para obtener los datos del estudiante
+		EntityResult result = this.daoHelper.query(this.studentDao, filter, columns);
+		if (result != null && result.calculateRecordNumber() > 0) {
+			return result.getRecordValues(0);
+		}
+		return null;
+	}*/
+
 	private EntityResult createErrorResult(String message) {
 		EntityResult error = new EntityResultMapImpl();
 		error.setCode(EntityResult.OPERATION_WRONG);
