bca2ea78a01c5c1e7295b88d1baa3132aa65f29d
eluiui <jesusmiguez@gmail.com>
2024-11-08 13:49:54 +0100

    solo queda que el maestro javi imparta su sabiduria de css

--- cd2024bfs4g1-frontend/src/main/ngx/src/app/main/students/students-detail/students-detail.component.ts
+++ cd2024bfs4g1-frontend/src/main/ngx/src/app/main/students/students-detail/students-detail.component.ts
@@ -1,5 +1,5 @@
 import { Component, Injector, ViewChild } from '@angular/core';
-import { OFormComponent, OntimizeService, OTableComponent, OTextInputComponent } from 'ontimize-web-ngx';
+import { OFileInputComponent, OFormComponent, OntimizeService, OTableComponent, OTextInputComponent } from 'ontimize-web-ngx';
 import { ODateInputComponent } from 'ontimize-web-ngx';
 import { FormControl, ValidationErrors, ValidatorFn } from '@angular/forms';
 import { Router } from '@angular/router';
@@ -14,6 +14,7 @@ import spainComunitys from 'src/app/main/students/spaincomunitys';
 export class StudentsDetailComponent {
   @ViewChild("idNumber") idNumber: OTextInputComponent;
   @ViewChild("documentsTable") documentsTable: OTableComponent;
+  @ViewChild("fileinput") fileinput: OFileInputComponent;
   validatorsArray: ValidatorFn[] = [];
   validatorsArray1: ValidatorFn[] = [];
   dataArray = spainComunitys.map(comunity => ({ key: comunity, value: comunity }));
@@ -83,6 +84,7 @@ export class StudentsDetailComponent {
 
   onUploadFiles(event) {
     this.documentsTable.refresh();
+    this.fileinput.clearValue();
     alert("File added")
 
 
