b591725de8fa13765df7e1bab249995ee379c8c2
Nicolás Vázquez <nikovazquez52@gmail.com>
2024-11-13 11:02:20 +0100

    eliminar tooltips

--- cd2024bfs4g1-frontend/src/main/ngx/src/app/shared/app.menu.config.ts
+++ cd2024bfs4g1-frontend/src/main/ngx/src/app/shared/app.menu.config.ts
@@ -3,15 +3,15 @@ import { MenuRootItem } from 'ontimize-web-ngx';
 export const MENU_CONFIG: MenuRootItem[] = [
   { id: 'home', name: 'HOME', icon: 'home', route: '/main/home' },
   { id: 'bootcamps', name: 'Bootcamps', icon: 'view_cozy', route: '/main/bootcamps' },
-  { id: 'students', name: 'Students', tooltip: 'STUDENTS', route: '/main/students', icon: 'person' },
+  { id: 'students', name: 'Students', route: '/main/students', icon: 'person' },
   {
-    id: 'admin', name: 'ADMIN', tooltip: 'ADMIN', icon: 'admin_panel_settings',
+    id: 'admin', name: 'ADMIN', icon: 'admin_panel_settings',
     items: [
-      { id: 'roles', name: 'ROLES', tooltip: 'ROLES', route: '/main/admin/roles', icon: 'supervisor_account' },
-      { id: 'users', name: 'USERS', tooltip: 'USERS', route: '/main/admin/users', icon: 'person' },
+      { id: 'roles', name: 'ROLES', route: '/main/admin/roles', icon: 'supervisor_account' },
+      { id: 'users', name: 'USERS', route: '/main/admin/users', icon: 'person' },
     ]
   },
-  { id: 'personalinfo', name: 'Mis Datos', tooltip: 'DATOS', route:'/main/data', icon:'article'},
-  {id: 'config', name: 'Config', tooltip: 'CONFIG', route: '/main/config', icon: 'tune' },
+  { id: 'personalinfo', name: 'Mis Datos', route:'/main/data', icon:'article'},
+  {id: 'config', name: 'Config', route: '/main/config', icon: 'tune' },
   { id: 'logout', name: 'LOGOUT', route: '/login', icon: 'power_settings_new', confirm: 'yes' }
 ];
