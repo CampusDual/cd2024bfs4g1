46774434c1d39e014f6273c8f0301ca41b229930
Ivan Silvosa Rodriguez <ivansrlsy@gmail.com>
2024-11-15 09:53:07 +0100

    Aviso del DNI duplicado

--- cd2024bfs4g1-frontend/src/main/ngx/src/app/main/students/students-new/students-new.component.ts
+++ cd2024bfs4g1-frontend/src/main/ngx/src/app/main/students/students-new/students-new.component.ts
@@ -1,5 +1,5 @@
-import { Component, ViewChild } from '@angular/core';
-import { OFormComponent, OValidators } from 'ontimize-web-ngx';
+import { Component,Injector, ViewChild } from '@angular/core';
+import { OFormComponent,OntimizeService, OValidators } from 'ontimize-web-ngx';
 import { ODateInputComponent } from 'ontimize-web-ngx';
 import { FormControl, ValidationErrors, ValidatorFn } from '@angular/forms';
 import spainComunitys from 'src/app/main/students/spaincomunitys';
@@ -12,7 +12,7 @@ import spainComunitys from 'src/app/main/students/spaincomunitys';
 export class StudentsNewComponent {
 
   @ViewChild('studentsform') protected formStudents: OFormComponent;
-
+  protected service: OntimizeService;
   validatorsArray: ValidatorFn[] = [];
   validatorsArray1: ValidatorFn[] = [];
   validatorsNewPasswordArray: ValidatorFn[] = [];
@@ -20,17 +20,29 @@ export class StudentsNewComponent {
 
 
   constructor() {
+  Mostraraviso: boolean =false;
+
+
+  constructor(protected injector: Injector) {
     this.validatorsArray.push(this.dateValidator);
     this.validatorsNewPasswordArray.push(OValidators.patternValidator(/\d/, 'hasNumber'));
     this.validatorsNewPasswordArray.push(OValidators.patternValidator(/[A-Z]/, 'hasCapitalCase'));
     this.validatorsNewPasswordArray.push(OValidators.patternValidator(/[a-z]/, 'hasSmallCase'));
     this.validatorsWithoutSpace.push(OValidators.patternValidator(/^(?!\s*$).+/, 'hasSpecialCharacters'));
+    this.service = this.injector.get(OntimizeService);
+    this.configureService();
   }
 
   dataArray = spainComunitys.map(comunity => ({ key: comunity, value: comunity }));
 
   // Valor predeterminado (opcional)
   valueSimple = "Madrid"; // Elige el valor que deseas predeterminar
+
+  protected configureService() {
+    // Configure the service using the configuration defined in the `app.services.config.ts` file
+    const conf = this.service.getDefaultServiceConfiguration('students');
+    this.service.configureService(conf);
+  }
 
   insertStudent() {
     this.formStudents.insert();
@@ -68,6 +80,34 @@ export class StudentsNewComponent {
   toUpperCase(event: any) {
     event.target.value = event.target.value.toUpperCase();
   }
+  mostrar(event: any) {
 
+    console.log(event.srcElement.value);
+    this.getDNI(event.srcElement.value);
+
+  }
 }
 
+getDNI(dni:string) {
+
+    const filter = {
+      'dni': dni
+    };
+    const columns = ['id'];
+    this.service.query(filter, columns, 'student').subscribe(resp => {
+      if (resp.code === 0){
+        console.log('DNI encontrado:');
+        console.log(resp.data.length);
+        if(!resp.data.length){
+          this.Mostraraviso=false;
+        }else{
+          this.Mostraraviso=true;
+        }
+      } else {
+        console.log(resp.message);
+        this.Mostraraviso=false;
+      }
+    });
+
+}
+}
