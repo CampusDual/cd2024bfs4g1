db6b6be874f7bc9f34f8da9c1dd026a85acae575
marcos.padin@outlook.es <you@example.com>
2024-11-18 09:25:45 +0100

    actualización v16 para resolución pre

---
+++ cd2024bfs4g1-model/src/main/resources/db/migration/V16__role_sv_permission.sql
@@ -0,0 +1,8 @@
+INSERT INTO public.usr_role_server_permission (rol_id,srp_id)
+	VALUES (3,1);
+INSERT INTO public.usr_role_server_permission (rol_id,srp_id)
+	VALUES (3,2);
+INSERT INTO public.usr_role_server_permission (rol_id,srp_id)
+	VALUES (2,3);
+INSERT INTO public.usr_role_server_permission (rol_id,srp_id)
+	VALUES (2,4);
