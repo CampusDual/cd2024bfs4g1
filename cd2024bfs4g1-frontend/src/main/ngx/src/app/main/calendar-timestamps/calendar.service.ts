7f5245ed4b17583b754039b98a665d0b281fe6f4
Velasco <quarantine.sleep480@passinbox.com>
2024-11-20 10:47:33 +0100

    Arreglos en CamelCase optimización de código.
    Months Updated
    
    Comments Deleted
    
    Change Bootcamp Name to "..." in specific cases
    
    Tooltip con saltos de lineas mejor hecho.
    
    tooltip regular
    
    css fixes
    
    Calendar-timestamp Weekends CSS
    
    Colores los fines de semana
    
    Faltan los colores en findes de semana
    
    Calendar-timestamp Css Changes
    
    Los bootcamps están ligeramente movidos.
    
    Calendar-timestamp initial day fixed
    
    Los bootocamp se apilan unos sobre otros
    
    Calendar-timestamp Css Actual Day Changes
    
    cambio de color flechas
    
    Direccionamiento al bootcamp, arreglado de indentado, que daba asco
    
    Direccionamiento al bootcamp
    
    bux: ahora los bootcamps cargan al entrar a la pagina, no hay que esperar a clicar en un boton
    
    feat: la letra dentro de los bootcamps cambia en funcion del tamaño de la tarjeta
    
    Calendar-timestamp Spike info added
    
    feat: archivos cronograma creados

---
+++ cd2024bfs4g1-frontend/src/main/ngx/src/app/main/calendar-timestamps/calendar.service.ts
@@ -0,0 +1,47 @@
+// src/app/services/bootcamp-search.service.ts
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CalendarService {
+  private apiUrl = 'http://localhost:8080/bootcamps/bootcamp/search';
+
+  constructor(private http: HttpClient) {}
+
+  searchBootcamps(): Observable<any> {
+    const requestBody = {
+      columns: ['id', 'name', 'start_date', 'end_date', 'status'],
+      filter: {},
+      sqltypes: {
+        start_date: 93,
+        end_date: 93
+      }
+    };
+
+    const headers = new HttpHeaders({
+      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; rv:124.0) Gecko/20100101 Firefox/124.0',
+      'Accept': 'application/json, text/plain, */*',
+      'Accept-Language': 'en-US,en;q=0.5',
+      'Accept-Encoding': 'gzip, deflate, br',
+      'Access-Control-Allow-Origin': '*',
+      'Content-Type': 'application/json;charset=UTF-8',
+      'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjcmVhdGlvbi10aW1lIjoxNzI4NjQzMTk1MDU1LCJ1c2VybmFtZSI6ImRlbW8ifQ.0zjaGgrFt2LQG76gYBadZj8_SbJed0DePS2rvBgtUxU',
+      'Origin': 'http://localhost:4299',
+      'DNT': '1',
+      'Connection': 'keep-alive',
+      'Referer': 'http://localhost:4299/',
+      'Sec-Fetch-Dest': 'empty',
+      'Sec-Fetch-Mode': 'cors',
+      'Sec-Fetch-Site': 'same-site'
+    });
+
+    return this.http.post<any>(this.apiUrl, requestBody, { headers });
+  }
+
+
+}
