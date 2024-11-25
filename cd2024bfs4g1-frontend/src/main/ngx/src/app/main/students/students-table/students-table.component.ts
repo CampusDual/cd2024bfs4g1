2a717642eed57b165b7d90ef4c86df2e272a9196
David Dominguez Vidal <daviddominguezvidaall@gmail.com>
2024-11-25 12:15:36 +0100

    Bug de redireccion logrado

--- cd2024bfs4g1-frontend/src/main/ngx/src/app/main/students/students-table/students-table.component.ts
+++ cd2024bfs4g1-frontend/src/main/ngx/src/app/main/students/students-table/students-table.component.ts
@@ -1,7 +1,7 @@
-import { Component} from '@angular/core';
-import { FilterExpressionUtils, Expression, OFormComponent } from 'ontimize-web-ngx';
+import { Component, ViewChild } from '@angular/core';
+import { FilterExpressionUtils, Expression, OFormComponent, OFormLayoutManagerComponent, OTableComponent } from 'ontimize-web-ngx';
 import spainComunitys from 'src/app/main/students/spaincomunitys';
-
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-students-table',
@@ -10,12 +10,57 @@ import spainComunitys from 'src/app/main/students/spaincomunitys';
 })
 export class StudentsTableComponent {
 
-  form!: OFormComponent;
 
+  @ViewChild('tableStudents') tableComponent: OTableComponent;
+  form!: OFormComponent;
   dataArray = spainComunitys.map(comunity => ({ key: comunity, value: comunity }));
+  detailId !: Number;
 
   // Valor predeterminado (opcional)
-  valueSimple = "Madrid"; // Elige el valor que deseas predeterminar
+  valueSimple = "Madrid";
+
+  constructor(private route: ActivatedRoute, private router: Router) { }
+
+  ngOnInit() {
+    this.route.queryParams.subscribe(params => {
+      const tab = params['tab'];
+      const studentId = params['studentId'];
+      if (studentId) {
+        this.detailId = studentId;
+      }
+    });
+  }
+
+  goToDetail() {
+    if (this.detailId) {
+      this.tableComponent.viewDetail({ id: this.detailId });
+
+    }
+
+
+  }
+
+
+  openStudentDetail(studentId: string) {
+    console.log(`Abriendo detalle del estudiante con ID: ${studentId}`);
+
+
+    const formLayoutManager = document.querySelector('o-form-layout-manager') as any;
+
+    if (formLayoutManager) {
+      formLayoutManager.openDetail({
+        entity: 'student',
+        keysValues: { id: studentId },
+
+
+
+      });
+    } else {
+      console.error('No se pudo encontrar el o-form-layout-manager');
+    }
+  }
+
+  // Método para construir el filtro (sin cambios)
   createFilter(values: Array<{ attr: string, value: any }>): Expression {
     let filters: Array<Expression> = [];
 
@@ -35,12 +80,11 @@ export class StudentsTableComponent {
     });
 
     if (filters.length > 0) {
-
-        return filters.reduce((exp1, exp2) => FilterExpressionUtils.buildComplexExpression(exp1, exp2, FilterExpressionUtils.OP_AND));
+      return filters.reduce((exp1, exp2) =>
+        FilterExpressionUtils.buildComplexExpression(exp1, exp2, FilterExpressionUtils.OP_AND)
+      );
     } else {
       return null;
     }
   }
-
-
 }
