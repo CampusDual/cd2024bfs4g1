8a670d2eed0032728904525a6275e2595e21035c
eluiui <jesusmiguez@gmail.com>
2024-11-26 11:51:25 +0100

    con errores

---
+++ cd2024bfs4g1-frontend/src/main/ngx/src/app/main/data/personal-tutor-info/bootcamp-students-table/bootcamp-students-table.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bootcamp-students-table',
+  templateUrl: './bootcamp-students-table.component.html',
+  styleUrls: ['./bootcamp-students-table.component.css']
+})
+export class BootcampStudentsTableComponent {
+
+}
