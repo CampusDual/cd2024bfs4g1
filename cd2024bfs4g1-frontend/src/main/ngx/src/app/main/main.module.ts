83aa66666f6a04ed3fcb86ca52dbb739febe4dcf
Gabriel Alonso Varela <gabrielalonsovarela1@gmail.com>
2024-12-03 12:29:24 +0100

    CommercialSection module added

--- cd2024bfs4g1-frontend/src/main/ngx/src/app/main/main.module.ts
+++ cd2024bfs4g1-frontend/src/main/ngx/src/app/main/main.module.ts
@@ -5,6 +5,7 @@ import { SharedModule } from '../shared/shared.module';
 import { MainRoutingModule } from './main-routing.module';
 import { MainComponent } from './main.component';
 import { ProfileComponent } from './profile/profile.component';
+import { CommercialSectionComponent } from './commercial-section/commercial-section.component';
 
 @NgModule({
   imports: [
@@ -14,7 +15,8 @@ import { ProfileComponent } from './profile/profile.component';
   ],
   declarations: [
     MainComponent,
-    ProfileComponent
+    ProfileComponent,
+    CommercialSectionComponent
   ]
 })
 export class MainModule { }
