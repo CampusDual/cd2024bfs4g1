e44adf9c1c4ee09af5c44ca05299050756506237
Nicolás Vázquez <nikovazquez52@gmail.com>
2024-12-04 10:57:08 +0100

    cambios en el service AttendanceControl

--- cd2024bfs4g1-model/src/main/java/com/campusdual/cd2024bfs4g1/model/core/service/AttendanceControlService.java
+++ cd2024bfs4g1-model/src/main/java/com/campusdual/cd2024bfs4g1/model/core/service/AttendanceControlService.java
@@ -7,10 +7,14 @@ import com.ontimize.jee.common.dto.EntityResult;
 import com.ontimize.jee.common.exceptions.OntimizeJEERuntimeException;
 import com.ontimize.jee.server.dao.DefaultOntimizeDaoHelper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Map;
 
+@Service("AttendanceControlService")
+@Lazy
 public class AttendanceControlService implements IAttendanceControlService {
 
     @Autowired
