3d2ac0d5cc0179b363076e36e0ea3cf98a481809
David Dominguez Vidal <daviddominguezvidaall@gmail.com>
2024-12-05 15:30:56 +0100

    Se muestran la lista de las notas de cada alumno

--- cd2024bfs4g1-model/src/main/java/com/campusdual/cd2024bfs4g1/model/core/service/NotesService.java
+++ cd2024bfs4g1-model/src/main/java/com/campusdual/cd2024bfs4g1/model/core/service/NotesService.java
@@ -1,17 +1,19 @@
 package com.campusdual.cd2024bfs4g1.model.core.service;
 
 import com.campusdual.cd2024bfs4g1.api.core.service.INotesService;
-import com.campusdual.cd2024bfs4g1.model.core.dao.EmploymentStatusDao;
+
 import com.campusdual.cd2024bfs4g1.model.core.dao.NotesDao;
 import com.ontimize.jee.common.dto.EntityResult;
 import com.ontimize.jee.common.exceptions.OntimizeJEERuntimeException;
 import com.ontimize.jee.server.dao.DefaultOntimizeDaoHelper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Map;
-
-public class NotesService implements INotesService {
+@Service("NotesService")
+@Lazy public class NotesService implements INotesService {
 
     @Autowired
     private NotesDao notesDao;
@@ -32,7 +34,7 @@ public class NotesService implements INotesService {
 
     @Override
     public EntityResult notesQuery(Map<String, Object> keyMap, List<String> attrList) {
-        return this.daoHelper.query(this.notesDao, keyMap, attrList, "documentfiles");
+        return this.daoHelper.query(this.notesDao, keyMap, attrList);
     }
 
 
