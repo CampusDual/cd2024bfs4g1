c8ea830ef45e4a3e6d175bf2bc1b9bac056aeda2
Juanjo <110014245+juanjo-alvarez@users.noreply.github.com>
2024-12-10 12:38:12 +0100

    Correccion flywey

---
+++ cd2024bfs4g1-model/src/main/resources/db/migration/V32__tutors_hidden_permision.sql
@@ -0,0 +1,5 @@
+INSERT into tmenu (id,attr,visible,enabled,id_rolename) VALUES ('23','tutors',true,true,1);
+
+INSERT into tmenu (id,attr,visible,enabled,id_rolename) VALUES ('24','tutors',true,true,2);
+
+INSERT into tmenu (id,attr,visible,enabled,id_rolename) VALUES ('25','tutors',false,false,3);
