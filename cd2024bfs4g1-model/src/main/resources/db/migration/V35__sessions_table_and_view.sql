252e3255c45397ae0d6aab5a040ba72c739f835f
Juanjo <110014245+juanjo-alvarez@users.noreply.github.com>
2024-12-12 11:09:40 +0100

    Flyway move view between scripts

--- cd2024bfs4g1-model/src/main/resources/db/migration/V35__sessions_table_and_view.sql
+++ cd2024bfs4g1-model/src/main/resources/db/migration/V35__sessions_table_and_view.sql
@@ -25,3 +25,13 @@ SELECT
         ELSE 'Pending'
     END AS status
 FROM sessions;
+
+CREATE OR REPLACE VIEW public.sessions_status
+AS SELECT sessions.id,sessions.id_bootcamp,sessions.session_name,sessions.session_date,sessions.link,sessions.password,
+        CASE
+            WHEN sessions.session_date::date < CURRENT_DATE THEN 'Finished'::text
+            WHEN sessions.session_date::date = CURRENT_DATE THEN 'Started'::text
+            ELSE 'Pending'::text
+        END AS status
+   FROM sessions
+   order by session_name collate "es-ES-x-icu";
