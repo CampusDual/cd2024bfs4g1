be2ca01bd994c13c4b3a17a495125deb39d8d15e
Juanjo <110014245+juanjo-alvarez@users.noreply.github.com>
2025-01-14 10:06:40 +0100

    Menu and routes view

---
+++ cd2024bfs4g1-frontend/src/main/ngx/src/app/main/admin/menu/menuhome/menuhome.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menuhome',
+  templateUrl: './menuhome.component.html',
+  styleUrls: ['./menuhome.component.css']
+})
+export class MenuhomeComponent {
+
+}
---
+++ cd2024bfs4g1-frontend/src/main/ngx/src/app/main/admin/routes/routeshome/routeshome.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-routeshome',
+  templateUrl: './routeshome.component.html',
+  styleUrls: ['./routeshome.component.css']
+})
+export class RouteshomeComponent {
+
+}
